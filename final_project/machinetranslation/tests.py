44fa6606552936b1cfd4a2c106a013642e6464d0
tssxember <youremail@domain.com>
2022-08-09 13:53:19 +0000

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,17 +5,11 @@ from  translator import french_to_english
 class TestModule(unittest.TestCase):
     def test_f2e(self):
         self.assertEqual(french_to_english('Bonjour'),'Hello')
-    def test_f2e2(self):
-        self.assertEqual(french_to_english('None'),'')
-    def test_f2e3(self):
         self.assertNotEqual(french_to_english(0),0)
 
 class TestModule2(unittest.TestCase):
     def test_e2f(self):
         self.assertEqual(english_to_french('Hello'),'Bonjour')
-    def test_e2f2(self):
-        self.assertEqual(english_to_french('None'),'')
-    def test_e2f3(self):
         self.assertNotEqual(english_to_french(0),0) 
 
 
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -5,19 +5,22 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
 load_dotenv()
-apikey = os.environ['apikey']
-url = os.environ['url']
-authenticator = IAMAuthenticator('u7B14Sz9byYUuaQGByxISTjLtHqARTEFdbxpQOM8hN1k')
-language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
 
-language_translator.set_service_url('https://api.us-east.language-translator.watson.cloud.ibm.com')
+authenticator = IAMAuthenticator('vZlvyp2UJVr8U8O2Njxa7kZOCjwYTGhObtNVmgV2fC4k')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator)
+
+language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/9d4dbfce-8724-40c1-93f7-5dc3e1548cac')
 
 def english_to_french(english_text):
-    translation= language_translator.translate(text=english_text, model_id="en-fr").get_result()
-    french_text=translation['translations'][0].get['translation']
-    return french_text
+    french_translation = language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()
+    return french_translation.get('translations')[0].get('translation')
 
 def french_to_english(french_text):
-    translation= language_translator.translate(text=french_text, model_id="fr-en").get_result()
-    english_text=translation['translations'][0].get['translation']
-    return english_text
+    english_translation = language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()
+    return english_translation.get('translations')[0].get('translation')
